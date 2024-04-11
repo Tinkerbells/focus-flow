eb956174682da15e0441f6f04e8c2605347dad4e
untermensch <untermenschH@protonmail.com>
2024-04-11 15:01:17 +0300

    add: add css color variables and lucide icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Camera } from 'lucide-react';
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <div>
+        <Camera color="white" size={48} />;
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
