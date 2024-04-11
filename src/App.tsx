c486558e5b72747ad6380783b5223ab4ac910601
untermensch <untermenschH@protonmail.com>
2024-04-11 19:47:00 +0300

    update: change lucide icons to vk

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Camera } from 'lucide-react';
+import { Icon24Cancel } from '@vkontakte/icons';
 import './App.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <Camera color="white" size={48} />;
+        <Icon24Cancel />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
