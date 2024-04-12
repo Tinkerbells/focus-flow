87c1c181802169275805a8e263d94cf0c2a99267
Alexandr Korovin <akorovn815@gmail.com>
2024-04-12 19:43:59 +0300

    add table

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { useState } from 'react'
-import { Icon24Cancel } from '@vkontakte/icons';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <Icon24Cancel />
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className='container'>
+      <div className='table grey'>To Do</div> 
+      <div className='table blue'>In progress</div> 
+      <div className='table green'>Done</div> 
       </div>
     </>
+      
   )
 }
 
