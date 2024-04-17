e12df5d965e7cb69979968e9b08cd14683697473
Alexandr Korovin <akorovn815@gmail.com>
2024-04-17 20:09:22 +0300

    add title and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Icon16PauseCircle } from "@vkontakte/icons";
 
 function App() {
   return (
     <>
-    <div className='container'>
-      <div className='table grey'>To Do</div> 
-      <div className='table blue'>In progress</div> 
-      <div className='table green'>Done</div> 
+      <div className="container">
+        <div className="table grey">
+          <div className="table__header">
+            <div className="title do">
+              <Icon16PauseCircle />
+              To Do
+            </div>
+            <div className="table__counter grey">3</div>
+          </div>
+        </div>
+        <div />
+        <div className="table blue">
+          <div className="title progress">In progress</div>
+        </div>
+        <div className="table green">
+          <div className="title done">Done</div>
+        </div>
       </div>
     </>
-      
-  )
+  );
 }
 
-export default App
+export default App;
